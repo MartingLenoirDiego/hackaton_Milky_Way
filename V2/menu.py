5f5dadc080263551798970674893490a5f159d53
Disdix10 <44231661+Disdix10@users.noreply.github.com>
2021-03-27 21:35:55 +0100

    restructuration git

--- V2/menu.py
+++ V2/menu.py
@@ -1,109 +1,59 @@
 import pygame
-import random
-import sys
-pygame.init()
-fps=30
-fpsclock=pygame.time.Clock()
-cadre_x = 600
-cadre_y = 400
-screen=pygame.display.set_mode((cadre_x, cadre_y))
-pygame.display.set_caption("Keyboard_Input")
-
-#couleur
-blanc=(255,255,255)
-noir=(0,0,0)
-vert=(0,255,0)
-
-#pas auquel le joueur se déplace
-step=10
-
-#ennemi
-class Ennemi:
-    def __init__(self):
-        self.x = 200
-        self.y = 300
-        self.img_ennemi = pygame.image.load('square.png')
-        self.nvlle_img_ennemi = pygame.transform.scale(self.img_ennemi, (50, 50)) 
-        self.rect_ennemi = self.nvlle_img_ennemi.get_rect()
-        self.rect_ennemi.topleft = (self.x, self.y)
-
-#joueur/fusée
-class Joueur:
-    def __init__(self):
-        self.x = 100
-        self.y = 335
-        self.img_joueur = pygame.image.load('perso.jpg')
-        self.nvlle_img_joueur = pygame.transform.scale(self.img_joueur, (50, 50)) 
-        self.rect_joueur = self.nvlle_img_joueur.get_rect()
-        self.rect_joueur.topleft = (self.x, self.y)
-        self.nbr_vies = 3
-        self.img_vie = pygame.image.load('coeur.png')
-
-compteur = 1
-
-def jeu():
-    fin = False
-    ennemi = Ennemi()
-    joueur = Joueur()
-    compteur = 1
-    while not fin:
-        screen.fill(noir)
-
-        for eve in pygame.event.get():
-            if eve.type==pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        if compteur%2 != 0 :
-            screen.blit(joueur.nvlle_img_joueur, (joueur.x,joueur.y))
-            joueur.rect_joueur.topleft = (joueur.x, joueur.y)
-        else :
-            screen.fill(noir)
+#from game import Game
+ 
+def begin():
+ 
+    pseudo = input("Enter a pseudoname: ")
+ 
+    pygame.init()
+ 
+    pygame.display.set_caption("Space Mars! ")
+    screen = pygame.display.set_mode((1080, 620))
+    background = pygame.image.load("im\marsim.jpg")
+    over = pygame.image.load("im\go.jpg")
+ 
+    img_but_play = pygame.image.load('im\pngegg.png')
+    but_play = pygame.transform.scale(img_but_play, (600, 750))
+    but_play_rect = but_play.get_rect()
+    but_play_rect.x = but_play.get_width() / 5
+    but_play_rect.x = but_play.get_height() / 0.5
+ 
+ 
+    replay = pygame.image.load('im\pngegg.png')
+    replay = pygame.transform.scale(but_play, (400, 150))
+    replay_rect = replay.get_rect()
+    replay_rect.x = replay.get_width() / 5
+    replay_rect.x = replay.get_height() / 0.5
+ 
+    game = Game()
+ 
+    running = True
+    while running:
+        screen.blit(background, (0, -400))
+        if game.is_playing and game.is_playing != "over":
+            game.update (screen)
+ 
+        elif game.is_playing == "over":
+            screen.blit(over, (0, -400))
+            #screen.blit(replay, replay_rect)
         
-        compteur = compteur - 1
-        if compteur < 1 :
-            compteur = 1
-
-        for i in range(joueur.nbr_vies) :
-            screen.blit(joueur.img_vie, (10 + i * 40, 10))
-
-        screen.blit(ennemi.nvlle_img_ennemi, (ennemi.x,ennemi.y))
-        ennemi.rect_ennemi.topleft = (ennemi.x, ennemi.y)
-
-        key_input = pygame.key.get_pressed()   
-        if key_input[pygame.K_LEFT]:
-            joueur.x -= step
-    
-        if key_input[pygame.K_RIGHT]:
-            joueur.x += step
-
-        if joueur.x > cadre_x-joueur.nvlle_img_joueur.get_size()[0] :
-            joueur.x = cadre_x-joueur.nvlle_img_joueur.get_size()[0]
+        else:
+            screen.blit(but_play, but_play_rect)
         
-        if joueur.x < 0 :
-            joueur.x = 0
-
-        if ennemi.rect_ennemi.colliderect(joueur.rect_joueur) :
-            ennemi.x = random.randrange(0, 600)
-            ennemi.y = random.randrange(0, 400)
-            compteur = 9
-            
-            joueur.nbr_vies = joueur.nbr_vies - 1
-            if joueur.nbr_vies == 0 :
-                fin = True
-
-        pygame.display.update()
-        fpsclock.tick(fps)
-
-def main():
-    while True:
-        jeu()
-        screen.fill(vert)
-        for eve in pygame.event.get():
-            if eve.type==pygame.QUIT:
+        pygame.display.flip()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
                 pygame.quit()
-                sys.exit()
-
-if __name__ == "__main__":
-    main()
+                print("See you soon " + pseudo)
+ 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if but_play_rect.collidepoint(event.pos) and not game.is_playing:
+                    game.start_game()
+                    #game.is_playing = True
+                if replay_rect.collidepoint(event.pos) and game.is_playing =="over":
+                    game.start_game()
+ 
     
+begin()
